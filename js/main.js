82f6c72561da7400f2257f436de62afa5b271e76
pel1780 <118698633+pel1780@users.noreply.github.com>
2022-12-21 22:36:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,42 +1,36 @@
-$(function () {
-    $('.header .right a').on('click', function () {
+$(function(){
+    $('.header .right a').on('click',function(){
         $('.header .gnb').toggleClass('on');
-        // gnb보이게 하는
         $('.header .right a').toggleClass('on');
-        // menu icon 바꾸기
     });
 
-    // var wh = $(window).height();
-    // console.log(wh)
-    // $(window).on('scroll', function () {
-    //     var sct = $(window).scrollTop();
-    //     var navH = $('.header').height();
-    //     console.log(sct)
-    //     if (sct > (wh - navH)) {
-    //         $('.header').css({ top: -(sct - (wh - navH)) })
-    //     } else {
-    //         $('.header').css({ top: 0 })
-    //     }
-    // }) 스크롤이 일정 부분이 되었을때 요소가 멈추는것(sticky처럼 작동함)
+    var lastScrollTop = 0, delta = 5; 
 
-    $(window).on('wheel', function (event) {
-        var wdY = event.originalEvent.deltaY;
-        if (wdY > 0) {
-            $('.header').addClass('on')
-        } else {
-            $('.header').removeClass('on')
-        }
-    })
-
-    $(window).on('scroll', function () {
-
+    $(window).on('scroll',function(event){
         var sct = $(window).scrollTop();
 
+        // if (Math.abs(lastScrollTop - st) <= delta) return;
+
+        if(sct >lastScrollTop && (lastScrollTop > 0)){
+            $('.header').addClass('on')
+        } else{
+            $('.header').removeClass('on')
+        };
+        lastScrollTop = sct;
+
+        
+    });
+
+    $(window).on('scroll',function(){
+        var sct = $(window).scrollTop();
         if (sct > 0) {
             $('.header').addClass('bg')
         } else {
             $('.header').removeClass('bg')
-        }
+        } ;
     })
+
+    
+
 
 })
